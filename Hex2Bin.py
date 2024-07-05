89aa6be8fa0e17b1ed9e640f4aedcc6b71a1c847
s0ca <peyre.mathias@gmail.com>
2024-07-05 16:06:33 +0200

    Update Hex2Bin.py
    refacto

--- Hex2Bin.py
+++ Hex2Bin.py
@@ -1,16 +1,17 @@
 import sys
 
-input_filename = sys.argv[1]
-output_filename = sys.argv[2]
+def convert_hex_to_bytes(input_filename, output_filename):
+    with open(input_filename, 'r') as file_in, open(output_filename, 'wb') as file_out:
+        outdata = bytearray()
+        for line in file_in:
+            data = line.split()
+            for d in data:
+                if len(d) == 1:
+                    d = "0" + d
+                outdata.extend(bytes.fromhex(d))
+        file_out.write(outdata)
 
-file_in = open(input_filename, 'r')
-file_out = open(output_filename, 'wb')
-
-outdata = bytes()
-for line in file_in:
-  data = line.split()
-  for d in data:
-    if len(d) == 1:
-      d = "0" + d
-    outdata += bytes.fromhex(d)
-file_out.write(outdata)
+if __name__ == "__main__":
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+    convert_hex_to_bytes(input_filename, output_filename)
